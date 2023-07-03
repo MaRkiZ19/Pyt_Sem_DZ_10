dc8e0ef6ae16e7b0a79e28724d02960d594f95cf
ваше_имя <grigorenko-marko@rambler.ru>
2023-07-03 23:48:05 +0300

    phone book in progress

---
+++ controller.py
@@ -0,0 +1,54 @@
+import view
+import model
+import text
+
+
+def start():
+    while True:
+        select = view.menu()
+        match select:
+            case 1:
+                if model.open_file():
+                    view.print_message(text.load_succesful)
+                else:
+                    view.print_message(text.error_load)
+            case 2:
+                if model.save_file():
+                    view.print_message(text.save_succesful)
+                else:
+                    view.print_message(text.error_save)
+            case 3:
+                view.show_contacts(model.phone_book, text.empty_book)
+            case 4:
+                new = view.add_contact()
+                model.add_contact(new)
+                view.print_message(text.add_successful(new.get('name')))
+            case 5:
+                word = view.search_word()
+                result = model.search(word)
+                view.show_contacts(result, text.empty_search(word))
+            case 6:
+                word = view.search_word()
+                result = model.search(word)
+                view.show_contacts(result, text.empty_search(word))
+                index_chnge = view.index_chnge()
+                test = view.change_contact(index_chnge, result)
+                while True:
+                    select = view.menu_smal()
+                    match select:
+                        case 1:
+                            model.change_name(test)
+                        case 2:
+                            pass
+                        case 3:
+                            pass
+                        case 4:
+                            print(text.menu_smal_exit)
+                            break
+            case 7:
+                pass
+            case 8:
+                view.print_message(text.end)
+                break
+            
+
---
+++ model.py
@@ -0,0 +1,66 @@
+import json
+
+phone_book = {}
+path = 'sem10\phone_book.json'
+
+def open_file():
+    global phone_book
+    try:
+        with open(path, 'r', encoding='UTF-8') as file:
+            phone_book=json.load(file, )
+        return True
+    except:
+        return False
+
+def save_file():
+    try:
+        with open(path, 'w', encoding='UTF-8') as file:
+            json.dump(phone_book, file, ensure_ascii=False)
+        return True
+    except:
+        return False
+
+
+
+def checl_id():
+    if phone_book:
+        return max(list(map(int, phone_book))) +1
+    return 1
+
+def add_contact(new: {int: dict[str, str]}):
+    contact = {checl_id(): new}
+    phone_book.update(contact)
+
+def search(word: str) -> dict[int:dict[str,str]]:
+    result={}
+    for index, contact in phone_book.items():
+        if word.lower() in ' '.join(contact.values()).lower():
+            result[index] = contact
+    return result
+
+def change_name(ctn):
+    print(ctn)
+    print(type(ctn))
+
+
+# def change(menu_smal, index):
+#     ch_cnt = {phone_book.get(index)}
+    # while True:
+    #     select = menu_smal
+    #     match select:
+    #         case 1:
+    #             ch_cnt['name'] = input('Введлите новое имя: ')
+    #             print(f'\n Контакт успешно изменен')
+    #         case 2:
+    #             ch_cnt['phone'] = input('Введлите новый номер: ')
+    #             print(f'\n Контакт успешно изменен')
+    #         case 3:
+    #             ch_cnt['coment'] = input('Введлите новвый комментарий: ')
+    #             print(f'\n Контакт успешно изменен')
+    #         case 4:
+    #             break
+    # print(f'\n Спасибо, приходите еще')
+    # print('='*200 +'\n')
+
+
+        
